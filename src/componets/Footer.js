c53a6525c7430498d468fb8c4db996a6701288ae
noblejos <iroghamajoseph@gmail.com>
2022-07-02 14:29:38 +0100

    v1

---
+++ src/componets/Footer.js
@@ -0,0 +1,12 @@
+import styles from '../styles/helper/Footer.module.css'
+
+
+
+
+export default function Footer() {
+  return (
+    <nav>
+        Footer
+    </nav>
+  )
+}
---
+++ src/pages/connect.js
@@ -0,0 +1,11 @@
+import styles from "../styles/connect.module.css"
+
+import React from 'react'
+
+export default function connect() {
+  return (
+    <div className={styles.container}>
+        connect
+    </div>
+  )
+}
