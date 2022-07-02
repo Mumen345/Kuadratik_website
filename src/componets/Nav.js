005ebad28e8d3295ab9e06eaf95ba9fdace8139f
noblejos <iroghamajoseph@gmail.com>
2022-07-03 00:01:05 +0100

    v1

--- src/componets/Nav.js
+++ src/componets/Nav.js
@@ -1,10 +1,33 @@
 import styles from '../styles/helper/Nav.module.css'
+import Link from "next/link"
+import { useRef } from 'react'
 
 
 export default function Nav() {
+const navRef= useRef()
+const showNavBar=()=>{
+  navRef.current.classList.toggle(`${styles.responsive_nav}`)
+}
   return (
     <nav>
-        Nav
+        <div className={styles.nav}> 
+        <img src="/images/logo.svg"/>
+            <ul ref={navRef} className={styles.flow}>
+                <li><Link href='/'>About</Link></li>
+                <li><Link href='/'>Services</Link></li>
+                <li className={styles.connect}><Link href='/'>Connect</Link></li>
+                
+                <img className={`${styles.nav_btn} ${styles.nav_close_btn}`} src="/images/close1.svg" 
+                style={{filter:'invert(100%)'}}
+                onClick={showNavBar}
+                />
+            </ul>
+            <img className={styles.nav_btn} 
+            src="/images/menu5.svg" 
+             style={{filter:'invert(100%)'}} 
+             onClick={showNavBar}
+             />
+        </div>
     </nav>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
+import Footer from '../componets/Footer'
+import Nav from '../componets/Nav'
+import Top from '../componets/Top'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <Nav/>
+      <Top/>
+      <Footer/>
     </div>
   )
 }
