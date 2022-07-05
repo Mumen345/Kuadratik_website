c1d8b7d5dea10125f24773a4e07895e36275639b
noblejos <iroghamajoseph@gmail.com>
2022-07-05 08:07:07 +0100

    again

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -16,7 +16,7 @@ export default function connect() {
               </label>
               <br />
 
-              <div class={styles.Iconinside}>
+              <div className={styles.Iconinside}>
                 <input type="email" placeholder="Email address" />
                 <img src="images/extra/contact-light.svg" alt="contactus img" />
               </div>
@@ -26,7 +26,7 @@ export default function connect() {
                 Email
               </label>
               <br />
-              <div class={styles.Iconinside}>
+              <div className={styles.Iconinside}>
                 <input type="email" placeholder="Email address" />
                 <img src="images/extra/message-box.svg" alt="contactus img" />
               </div>
