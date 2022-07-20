4f4a1a865eaef9f5d0ec97c6c1236bff378dfd88
Mumen Elijah <kingxosas@gmail.com>
2022-07-20 16:55:02 +0100

    done the meta tage and the favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,21 @@
 import "../styles/globals.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import React from "react";
+import { Helmet } from "react-helmet";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Kuadratik</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Tag Line" />
+        <link rel="canonical" href="http://kuadratik.com" />
+      </Helmet>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
 export default MyApp;
