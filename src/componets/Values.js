a4917e5e0ff512b970189d821d7af57d1122e605
Mumen Elijah <kingxosas@gmail.com>
2022-07-22 22:57:44 +0100

    Finished everything

--- src/componets/Values.js
+++ src/componets/Values.js
@@ -3,6 +3,21 @@ import styles from "../styles/helper/Values.module.css";
 export default function Values() {
   return (
     <div className={styles.container}>
+      <div className={styles.values_div}>
+        <h1>Our Values</h1>
+        <p>
+          Create digital solutions that elevate the customer’s experience, shape
+          cultural landscapes and get people connected globally.
+        </p>
+        <div className={styles.ol}>
+          <ol>
+            <li>Quality</li>
+            <li>Quality</li>
+            <li>Adaptability</li>
+            <li>Excellence</li>
+          </ol>
+        </div>
+      </div>
       <img src="/images/our_values.gif" className={styles.value} />
     </div>
   );
